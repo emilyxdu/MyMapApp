74de652aad64fef06d1571c75b3bedeef449ad3a
Emily Du <emilyxdu@gmail.com>
2017-05-25 09:26:10 -0700

    Fixed markers and edited activity_maps.xml
    Marker locations odd; need to add streetview

--- MyMapApp/app/src/main/java/com/example/emily/mymapapp/MapsActivity.java
+++ MyMapApp/app/src/main/java/com/example/emily/mymapapp/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.emily.mymapapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -39,12 +43,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in SanFran and move the camera
-        LatLng sanfran = new LatLng(38, 122);
+        LatLng sanfran = new LatLng(37.77, 238.59);
         mMap.addMarker(new MarkerOptions().position(sanfran).title("Born here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sanfran));
 
-        LatLng sanDiego = new LatLng(37, 117);
+        LatLng sanDiego = new LatLng(32.7, 243.84);
         mMap.addMarker(new MarkerOptions().position(sanDiego).title("Marker in current location, San Diego"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sanDiego));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            mMap.setMyLocationEnabled(true);
+        }
+
+        else{
+            Log.d("Location", "Permission for location not granted");
+        }
+
     }
 }
