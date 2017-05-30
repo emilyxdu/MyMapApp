48b9db7e8eb51fcffa06e20661962933e2625d84
Emily Du <emilyxdu@gmail.com>
2017-05-30 09:28:26 -0700

    Completed switchView, worked on getLocation

--- MyMapApp/app/src/main/java/com/example/emily/mymapapp/MapsActivity.java
+++ MyMapApp/app/src/main/java/com/example/emily/mymapapp/MapsActivity.java
@@ -2,10 +2,13 @@ package com.example.emily.mymapapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,12 +17,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_NORMAL;
 import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_SATELLITE;
 import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_TERRAIN;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private boolean isGPSenabled = false;
+    private boolean isNetWorkEnabled = false;
+    private boolean canGetLocation = false;
+    private static final long MIN_TIME_BW_UPDATES = 1000*15*1;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,18 +78,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         mMap.setMyLocationEnabled(true);
-
     }
 
-    public void switchView (GoogleMap googleMap) {
-        if (mMap.getMapType() == (MAP_TYPE_SATELLITE)) {
-            mMap.setMapType(MAP_TYPE_TERRAIN);
+    public void switchView (View v) {
+        if (mMap.getMapType() == (GoogleMap.MAP_TYPE_SATELLITE)) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
 
-        if (mMap.getMapType() == (MAP_TYPE_TERRAIN)) {
-            mMap.setMapType(MAP_TYPE_SATELLITE);
+        else if (mMap.getMapType() == (GoogleMap.MAP_TYPE_NORMAL)) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+    }
+
+
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            //get GPS status
+            isGPSenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if(isGPSenabled)
+                Log.d("MyMaps", "getLocation: GPS is enabled");
+
+            //get network status
+            isNetWorkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if(isNetWorkEnabled)
+                Log.d("MyMaps", "getLocation: NETWORK is enabled");
+
+            if(!isGPSenabled && !isNetWorkEnabled) {
+                Log.d("MyMaps", "getLocation: No provider is enabled");
+            }
+
+            else {
+                this.canGetLocation = true;
+
+                if(isNetWorkEnabled) {
+                    Log.d("MyMaps", "getLocation: Network enabled - requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerNetwork);
+
+                    Log.d("MyMaps", "getLocation: NetworkLoc update request successful.");
+                    Toast.makeText(this, "Using Network", Toast.LENGTH_SHORT);
+                }
+
+                if(isGPSenabled) {
+                    Log.d("MyMaps", "getLocation: GPS enabled - requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                            locationListenerGPS);
+
+                    Log.d("MyMaps", "getLocation: GPSLoc update request successful.");
+                    Toast.makeText(this, "Using GPS", Toast.LENGTH_SHORT);
+                }
+
+            }
         }
 
-
+        catch (Exception e) {
+            Log.d("MyMaps", "Caught exception in getLocation");
+            e.printStackTrace();
+        }
     }
 }
